7b996ef3de8c68ed740f119bcb4c814f6100c3ed
Jose Miguel Valiente <valiente_jose2@hotmail.com>
2021-01-13 17:36:42 +0100

    Refactor comments section

--- app/utils/functions/firebase.js
+++ app/utils/functions/firebase.js
@@ -19,8 +19,11 @@ export const firebaseFn = {
     },
 
     getUserId: function () {
-        const user = firebase.auth().currentUser.uid || false
-        return user
+        return firebase.auth().currentUser.uid || false
+    },
+
+    getUser: function () {
+        return firebase.auth().currentUser
     },
 
     getFavorites: function (idTapas) {
@@ -72,6 +75,52 @@ export const firebaseFn = {
                     setIsLoading(false)
                 })
             })
+    },
+
+    getComments: function (idTapa, setComments) {
+        db.collection("comments")
+            .where("idTapa", "==", idTapa)
+            .get()
+            .then((res) => {
+                const initialComments = []
+                res.forEach((el) => {
+                    initialComments.push(el.data())
+                })
+                setComments(initialComments)
+            })
+    },
+
+    postComment: function (payload, setIsLoading, setError) {
+        db.collection("comments")
+            .add(payload)
+            .then(() => {
+                setIsLoading(false)
+            })
+            .catch(() => {
+                setIsLoading(false)
+                setError({ ...error, msg: "No se ha podido enviar el comentario" })
+            })
+    },
+
+    updateRating: function (idTapa, comment, setIsLoading, navigation) {
+        const tapaRef = db.collection("tapas").doc(idTapa)
+        tapaRef.get().then((res) => {
+            const tapaData = res.data()
+            const ratingTotal = tapaData.ratingTotal + comment.rating
+            const quantityVote = tapaData.quantityVote + 1
+            const ratingResult = ratingTotal / quantityVote
+            tapaRef.update({
+                rating: ratingResult,
+                ratingTotal,
+                quantityVote
+            }).then(() => {
+                setIsLoading(false)
+                navigation.goBack()
+            }).catch(() => {
+                setIsLoading(false)
+            })
+
+        })
     }
 
 }
